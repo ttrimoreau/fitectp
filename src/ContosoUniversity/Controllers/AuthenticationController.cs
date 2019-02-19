8748d2f58f4518f1533cd4d6a3cbb2b8ea86631b
Erik Lallemand <elallemand@talentsoft.com>
2019-02-19 13:57:12 +0100

    Commit avant de récupérer le master avec les modifications de structure de la DB et les viewmodels

---
+++ src/ContosoUniversity/Controllers/AuthenticationController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.BusinessLayer;
+
+namespace ContosoUniversity.Controllers
+{
+
+    public class AuthenticationController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+
+        // GET: Authentication
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Login(VMLogin vmlogin)
+        {
+            if (vmlogin.isvalid)
+            {
+                string HashedAndSaltedPassword = Authentication.SaltAndHash(vmlogin.Password);
+                if(db.People.Any(x => x.UserName == vmlogin.UserName && x.Password == HashedAndSaltedPassword))
+                {
+                    Session["UserId"] = db.People.Find(x => x.UserName == vmlogin.UserName).ID;
+                }
+            }
+            return View();
+        }
+
+        // GET: Authentication
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: Authentication
+        [HttpPost]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
