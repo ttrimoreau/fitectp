3acadd87c25f5f4a6e1f6eca85e3e5482bb5e119
Erik Lallemand <elallemand@talentsoft.com>
2019-02-19 13:57:12 +0100

    Commit avant de récupérer le master avec les modifications de structure de la DB et les viewmodels

---
+++ src/ContosoUniversity/BusinessLayer/Authentication.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ContosoUniversity.BusinessLayer
+{
+    public static class Authentication
+    {
+        public const string SALT = "Contoso";
+
+        //Returns the salted and hashed string of inputString (usual use: salted and hashed password)
+        public static string SaltAndHash(string inputString)
+        {
+            string SaltedString = string.Concat(SALT, inputString);
+            return sha256_hash(SaltedString);
+        }
+
+        //Returns the SHA256 hash of a given input string
+        public static string sha256_hash(string value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                Byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (Byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+    }
+}
