d24b713be64ed4dd45718f25fd30ac403cbad2e9
Clément Dudouyt <cdudouyt@talentsoft.com>
2019-02-19 13:45:29 +0100

    Mise à jour du model de données.
    Update Person - Ajout Table File Image
    Update table Course - Ajout table Lessons

---
+++ src/ContosoUniversity/Models/Lessons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+
+    public enum Day
+    {
+        Lundi,
+        Mardi,
+        Mercredi,
+        Jeudi,
+        Vendredi
+    }
+
+    public class Lessons
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public Day Day { get; set; }
+        //[DataType(DataType.Time)]
+        public DateTime HourStart { get; set; }
+        public int Duration { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}
