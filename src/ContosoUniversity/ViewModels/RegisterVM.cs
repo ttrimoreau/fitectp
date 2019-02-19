37556b0f6f76358a489e5f584827382018f009d5
Tomy Trimoreau <ttrimoreau@talentsoft.com>
2019-02-19 13:57:02 +0100

    Create : Squelette des controlleurs de tests

---
+++ src/ContosoUniversity/ViewModels/RegisterVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [RegularExpression("^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$", 
+            ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        public string Role { get; set; }
+
+        public string  UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        //prop à créer pour l'upload d'une photo de profil
+
+        
+
+    }
+}
