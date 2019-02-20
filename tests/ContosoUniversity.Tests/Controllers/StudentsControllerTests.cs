3390a16d21e6f29ae05644c967738698e9ea7e20
spetrolia <spetrolia@LP-JS185S2>
2019-02-20 12:23:50 +0100

    api students set up

--- tests/ContosoUniversity.Tests/Controllers/StudentsControllerTests.cs
+++ tests/ContosoUniversity.Tests/Controllers/StudentsControllerTests.cs
@@ -5,7 +5,10 @@ using ContosoUniversity.Models;
 using ContosoUniversity.Tests.Tools;
 using Moq;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -34,6 +37,35 @@ namespace ContosoUniversity.Tests.Controllers
         [Test]
         public void APIStudent_Success()
         {
+            int testID = 100;
+            string testLastName = "Petrolia";
+            string testFirstName = "Matoula";
+            DateTime testEnrollmentDate = DateTime.Now;
+            Enrollment testEnrollment1 = new Enrollment()
+            {
+                EnrollmentID = 1,
+                StudentID = testID,
+                CourseID = 100
+            };
+            Enrollment testEnrollment2 = new Enrollment()
+            {
+                EnrollmentID = 2,
+                StudentID = testID,
+                CourseID = 200
+            };
+            List<Enrollment> testEnrollments = new List<Enrollment>();
+            testEnrollments.Add(testEnrollment1);
+            testEnrollments.Add(testEnrollment2);
+
+            EntityGenerator generator = new EntityGenerator(dbContext);
+            Student student = generator.CreateStudentFull(testID, testLastName, testFirstName, testEnrollmentDate, testEnrollments);
+
+            IHttpActionResult okResult = controllerToTest.GetStudent(100);
+            //var contentResult =
+
+            //Assert.IsNotNull(contentResult);
+            
+            
             //type of file, json or xml?
             //not null
             //action result? - verifier url?
