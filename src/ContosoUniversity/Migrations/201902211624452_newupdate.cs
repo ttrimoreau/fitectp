c6d61d9c923b38fe6b4891c2a36ae6d5e8bf1be0
Vincent Boursicot <vboursicot@talentsoft.com>
2019-02-21 17:26:02 +0100

    modif base de donnée

--- src/ContosoUniversity/Migrations/201902211624452_newupdate.cs
+++ src/ContosoUniversity/Migrations/201902211624452_newupdate.cs
@@ -3,7 +3,7 @@ namespace ContosoUniversity.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class updateDB : DbMigration
+    public partial class newupdate : DbMigration
     {
         public override void Up()
         {
@@ -27,28 +27,28 @@ namespace ContosoUniversity.Migrations
                     {
                         ID = c.Int(nullable: false, identity: true),
                         Day = c.Int(nullable: false),
+                        CourseID = c.Int(nullable: false),
                         HourStart = c.DateTime(nullable: false),
                         Duration = c.Int(nullable: false),
-                        Course_CourseID = c.Int(),
                     })
                 .PrimaryKey(t => t.ID)
-                .ForeignKey("dbo.Course", t => t.Course_CourseID)
-                .Index(t => t.Course_CourseID);
+                .ForeignKey("dbo.Course", t => t.CourseID, cascadeDelete: true)
+                .Index(t => t.CourseID);
             
             AddColumn("dbo.Course", "StartDate", c => c.DateTime(nullable: false));
             AddColumn("dbo.Person", "UserName", c => c.String(nullable: false, maxLength: 15));
-            AddColumn("dbo.Person", "PassWord", c => c.String(nullable: false, maxLength: 64));
+            AddColumn("dbo.Person", "Password", c => c.String(nullable: false, maxLength: 64));
             AddColumn("dbo.Person", "Email", c => c.String(nullable: false, maxLength: 30));
         }
         
         public override void Down()
         {
-            DropForeignKey("dbo.Lessons", "Course_CourseID", "dbo.Course");
+            DropForeignKey("dbo.Lessons", "CourseID", "dbo.Course");
             DropForeignKey("dbo.FileImage", "PersonID", "dbo.Person");
-            DropIndex("dbo.Lessons", new[] { "Course_CourseID" });
+            DropIndex("dbo.Lessons", new[] { "CourseID" });
             DropIndex("dbo.FileImage", new[] { "PersonID" });
             DropColumn("dbo.Person", "Email");
-            DropColumn("dbo.Person", "PassWord");
+            DropColumn("dbo.Person", "Password");
             DropColumn("dbo.Person", "UserName");
             DropColumn("dbo.Course", "StartDate");
             DropTable("dbo.Lessons");
