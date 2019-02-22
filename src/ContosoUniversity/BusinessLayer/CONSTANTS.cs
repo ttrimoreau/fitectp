983fdad085f3a59b51c7e98b90d9ee5a74b83246
Tomy Trimoreau <ttrimoreau@talentsoft.com>
2019-02-22 12:17:18 +0100

    Creation of Constants file to store strings

--- src/ContosoUniversity/BusinessLayer/CONSTANTS.cs
+++ src/ContosoUniversity/BusinessLayer/CONSTANTS.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ContosoUniversity.BussinessLayer
+namespace ContosoUniversity.BusinessLayer
 {
     public static class CONSTANTS
     {
